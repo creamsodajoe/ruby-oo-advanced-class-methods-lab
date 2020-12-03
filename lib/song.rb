2c84fee8ecd2985da387ee2943dca3957e78cace
creamsodajoe <velasco.jordan.r@gmail.com>
2020-12-03 15:31:46 -0600

    commit tree ba37cccae59b3590e7465cbdcc5610d208bdd58c

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,51 @@ class Song
   end
 
   def save
-    self.class.all << self
+    @@all << self
+  end
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+  def self.find_by_name(name)
+    self.all.find do |song|
+      if song.name == name
+        song
+      end
+    end
+  end
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+  def self.alphabetical
+    self.all.sort_by {|song| song.name }
+  end
+  def self.new_from_filename(filename)
+    track = filename.split(/( - |\.)/)
+    new_song = self.new
+    new_song.name = track[2]
+    new_song.artist_name = track [0]
+    new_song
+  end
+  def self.create_from_filename(filename)
+    new_song = self.new.new_from_filename(filename)
+    self.all << new_song
+    new_song.save
   end
 
+  def self.destroy_all
+    @@all = []
+  end
 end
